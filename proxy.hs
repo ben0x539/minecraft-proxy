3331b022aedecffe9711889b37d297803df63bd1
Benjamin Herr <ben@0x539.de>
2010-11-16 20:34:41 +0100

    november update

--- proxy.hs
+++ proxy.hs
@@ -112,6 +112,7 @@ data PacketChat             = PacketChat PrefixString
 data PacketUpdateTime       = PacketUpdateTime Int64
 data PacketPlayerInventory  = PacketPlayerInventory Int32 InventoryArray
 data PacketSpawnPosition    = PacketSpawnPosition Int32 Int32 Int32
+data PacketUseEntity        = PacketUseEntity Int32 Int32
 data PacketFlying           = PacketFlying Bool
 data PacketPlayerPosition   = PacketPlayerPosition Float64be Float64be Float64be Float64be Bool
 data PacketPlayerLook       = PacketPlayerLook Float32be Float32be Bool
@@ -126,12 +127,14 @@ data PacketPickupSpawn      = PacketPickupSpawn Int32 Int16 Int8 Int32 Int32 Int
 data PacketCollectItem      = PacketCollectItem Int32 Int32
 data PacketVehicleSpawn     = PacketVehicleSpawn Int32 Int8 Int32 Int32 Int32
 data PacketMobSpawn         = PacketMobSpawn Int32 Int8 Int32 Int32 Int32 Int8 Int8
+data PacketEntityVelocity   = PacketEntityVelocity Int32 Int16 Int16 Int16
 data PacketDestroyEntity    = PacketDestroyEntity Int32
 data PacketEntity           = PacketEntity Int32
 data PacketEntityMove       = PacketEntityMove Int32 Int8 Int8 Int8
 data PacketEntityLook       = PacketEntityLook Int32 Int8 Int8
 data PacketEntityMoveLook   = PacketEntityMoveLook Int32 Int8 Int8 Int8 Int8 Int8
 data PacketEntityTeleport   = PacketEntityTeleport Int32 Int32 Int32 Int32 Int8 Int8
+data PacketAttachEntity     = PacketAttachEntity Int32 Int32
 data PacketPreChunk         = PacketPreChunk Int32 Int32 Bool
 data PacketMapChunk         = PacketMapChunk Int32 Int16 Int32 Int8 Int8 Int8 (PrefixByteArray Int32)
 data PacketMultiBlockChange = PacketMultiBlockChange Int32 Int32 BlockChangeArray
@@ -146,6 +149,7 @@ deriving instance Show PacketChat
 deriving instance Show PacketUpdateTime
 deriving instance Show PacketPlayerInventory
 deriving instance Show PacketSpawnPosition
+deriving instance Show PacketUseEntity
 deriving instance Show PacketFlying
 deriving instance Show PacketPlayerPosition
 deriving instance Show PacketPlayerLook
@@ -160,12 +164,14 @@ deriving instance Show PacketPickupSpawn
 deriving instance Show PacketCollectItem
 deriving instance Show PacketVehicleSpawn
 deriving instance Show PacketMobSpawn
+deriving instance Show PacketEntityVelocity
 deriving instance Show PacketDestroyEntity
 deriving instance Show PacketEntity
 deriving instance Show PacketEntityMove
 deriving instance Show PacketEntityLook
 deriving instance Show PacketEntityMoveLook
 deriving instance Show PacketEntityTeleport
+deriving instance Show PacketAttachEntity
 deriving instance Show PacketPreChunk
 deriving instance Show PacketMapChunk
 deriving instance Show PacketMultiBlockChange
@@ -180,6 +186,7 @@ deriving instance Read PacketChat
 deriving instance Read PacketUpdateTime
 deriving instance Read PacketPlayerInventory
 deriving instance Read PacketSpawnPosition
+deriving instance Read PacketUseEntity
 deriving instance Read PacketFlying
 deriving instance Read PacketPlayerPosition
 deriving instance Read PacketPlayerLook
@@ -194,12 +201,14 @@ deriving instance Read PacketPickupSpawn
 deriving instance Read PacketCollectItem
 deriving instance Read PacketVehicleSpawn
 deriving instance Read PacketMobSpawn
+deriving instance Read PacketEntityVelocity
 deriving instance Read PacketDestroyEntity
 deriving instance Read PacketEntity
 deriving instance Read PacketEntityMove
 deriving instance Read PacketEntityLook
 deriving instance Read PacketEntityMoveLook
 deriving instance Read PacketEntityTeleport
+deriving instance Read PacketAttachEntity
 deriving instance Read PacketPreChunk
 deriving instance Read PacketMapChunk
 deriving instance Read PacketMultiBlockChange
@@ -221,6 +230,7 @@ instance Binary Packet where
         _ | tag == packetTag (undefined :: PacketUpdateTime)       -> Packet . (get :: Get PacketUpdateTime)
         _ | tag == packetTag (undefined :: PacketPlayerInventory)  -> Packet . (get :: Get PacketPlayerInventory)
         _ | tag == packetTag (undefined :: PacketSpawnPosition)    -> Packet . (get :: Get PacketSpawnPosition)
+        _ | tag == packetTag (undefined :: PacketUseEntity)        -> Packet . (get :: Get PacketUseEntity)
         _ | tag == packetTag (undefined :: PacketFlying)           -> Packet . (get :: Get PacketFlying)
         _ | tag == packetTag (undefined :: PacketPlayerPosition)   -> Packet . (get :: Get PacketPlayerPosition)
         _ | tag == packetTag (undefined :: PacketPlayerLook)       -> Packet . (get :: Get PacketPlayerLook)
@@ -235,12 +245,14 @@ instance Binary Packet where
         _ | tag == packetTag (undefined :: PacketCollectItem)      -> Packet . (get :: Get PacketCollectItem)
         _ | tag == packetTag (undefined :: PacketVehicleSpawn)     -> Packet . (get :: Get PacketVehicleSpawn)
         _ | tag == packetTag (undefined :: PacketMobSpawn)         -> Packet . (get :: Get PacketMobSpawn)
+        _ | tag == packetTag (undefined :: PacketEntityVelocity)   -> Packet . (get :: Get PacketEntityVelocity)
         _ | tag == packetTag (undefined :: PacketDestroyEntity)    -> Packet . (get :: Get PacketDestroyEntity)
         _ | tag == packetTag (undefined :: PacketEntity)           -> Packet . (get :: Get PacketEntity)
         _ | tag == packetTag (undefined :: PacketEntityMove)       -> Packet . (get :: Get PacketEntityMove)
         _ | tag == packetTag (undefined :: PacketEntityLook)       -> Packet . (get :: Get PacketEntityLook)
         _ | tag == packetTag (undefined :: PacketEntityMoveLook)   -> Packet . (get :: Get PacketEntityMoveLook)
         _ | tag == packetTag (undefined :: PacketEntityTeleport)   -> Packet . (get :: Get PacketEntityTeleport)
+        _ | tag == packetTag (undefined :: PacketAttachEntity)     -> Packet . (get :: Get PacketAttachEntity)
         _ | tag == packetTag (undefined :: PacketPreChunk)         -> Packet . (get :: Get PacketPreChunk)
         _ | tag == packetTag (undefined :: PacketMapChunk)         -> Packet . (get :: Get PacketMapChunk)
         _ | tag == packetTag (undefined :: PacketMultiBlockChange) -> Packet . (get :: Get PacketMultiBlockChange)
@@ -260,6 +272,7 @@ instance Read Packet where
           _ | startsWith "PacketUpdateTime"       -> makePacket . (reads :: ReadS PacketUpdateTime)
           _ | startsWith "PacketPlayerInventory"  -> makePacket . (reads :: ReadS PacketPlayerInventory)
           _ | startsWith "PacketSpawnPosition"    -> makePacket . (reads :: ReadS PacketSpawnPosition)
+          _ | startsWith "PacketUseEntity"        -> makePacket . (reads :: ReadS PacketUseEntity)
           _ | startsWith "PacketFlying"           -> makePacket . (reads :: ReadS PacketFlying)
           _ | startsWith "PacketPlayerPosition"   -> makePacket . (reads :: ReadS PacketPlayerPosition)
           _ | startsWith "PacketPlayerLook"       -> makePacket . (reads :: ReadS PacketPlayerLook)
@@ -274,12 +287,14 @@ instance Read Packet where
           _ | startsWith "PacketCollectItem"      -> makePacket . (reads :: ReadS PacketCollectItem)
           _ | startsWith "PacketVehicleSpawn"     -> makePacket . (reads :: ReadS PacketVehicleSpawn)
           _ | startsWith "PacketMobSpawn"         -> makePacket . (reads :: ReadS PacketMobSpawn)
+          _ | startsWith "PacketEntityVelocity"   -> makePacket . (reads :: ReadS PacketEntityVelocity)
           _ | startsWith "PacketDestroyEntity"    -> makePacket . (reads :: ReadS PacketDestroyEntity)
           _ | startsWith "PacketEntity"           -> makePacket . (reads :: ReadS PacketEntity)
           _ | startsWith "PacketEntityMove"       -> makePacket . (reads :: ReadS PacketEntityMove)
           _ | startsWith "PacketEntityLook"       -> makePacket . (reads :: ReadS PacketEntityLook)
           _ | startsWith "PacketEntityMoveLook"   -> makePacket . (reads :: ReadS PacketEntityMoveLook)
           _ | startsWith "PacketEntityTeleport"   -> makePacket . (reads :: ReadS PacketEntityTeleport)
+          _ | startsWith "PacketAttachEntity"     -> makePacket . (reads :: ReadS PacketAttachEntity)
           _ | startsWith "PacketPreChunk"         -> makePacket . (reads :: ReadS PacketPreChunk)
           _ | startsWith "PacketMapChunk"         -> makePacket . (reads :: ReadS PacketMapChunk)
           _ | startsWith "PacketMultiBlockChange" -> makePacket . (reads :: ReadS PacketMultiBlockChange)
@@ -301,6 +316,7 @@ $(derive makeBinary ''PacketChat)
 $(derive makeBinary ''PacketUpdateTime)
 $(derive makeBinary ''PacketPlayerInventory)
 $(derive makeBinary ''PacketSpawnPosition)
+$(derive makeBinary ''PacketUseEntity)
 $(derive makeBinary ''PacketFlying)
 $(derive makeBinary ''PacketPlayerPosition)
 $(derive makeBinary ''PacketPlayerLook)
@@ -315,12 +331,14 @@ $(derive makeBinary ''PacketPickupSpawn)
 $(derive makeBinary ''PacketCollectItem)
 $(derive makeBinary ''PacketVehicleSpawn)
 $(derive makeBinary ''PacketMobSpawn)
+$(derive makeBinary ''PacketEntityVelocity)
 $(derive makeBinary ''PacketDestroyEntity)
 $(derive makeBinary ''PacketEntity)
 $(derive makeBinary ''PacketEntityMove)
 $(derive makeBinary ''PacketEntityLook)
 $(derive makeBinary ''PacketEntityMoveLook)
 $(derive makeBinary ''PacketEntityTeleport)
+$(derive makeBinary ''PacketAttachEntity)
 $(derive makeBinary ''PacketPreChunk)
 $(derive makeBinary ''PacketMapChunk)
 $(derive makeBinary ''PacketMultiBlockChange)
@@ -335,6 +353,7 @@ instance AnyPacket PacketChat             where packetTag = const 0x03
 instance AnyPacket PacketUpdateTime       where packetTag = const 0x04
 instance AnyPacket PacketPlayerInventory  where packetTag = const 0x05
 instance AnyPacket PacketSpawnPosition    where packetTag = const 0x06
+instance AnyPacket PacketUseEntity        where packetTag = const 0x07
 instance AnyPacket PacketFlying           where packetTag = const 0x0a
 instance AnyPacket PacketPlayerPosition   where packetTag = const 0x0b
 instance AnyPacket PacketPlayerLook       where packetTag = const 0x0c
@@ -349,12 +368,14 @@ instance AnyPacket PacketPickupSpawn      where packetTag = const 0x15
 instance AnyPacket PacketCollectItem      where packetTag = const 0x16
 instance AnyPacket PacketVehicleSpawn     where packetTag = const 0x17
 instance AnyPacket PacketMobSpawn         where packetTag = const 0x18
+instance AnyPacket PacketEntityVelocity   where packetTag = const 0x1c
 instance AnyPacket PacketDestroyEntity    where packetTag = const 0x1d
 instance AnyPacket PacketEntity           where packetTag = const 0x1e
 instance AnyPacket PacketEntityMove       where packetTag = const 0x1f
 instance AnyPacket PacketEntityLook       where packetTag = const 0x20
 instance AnyPacket PacketEntityMoveLook   where packetTag = const 0x21
 instance AnyPacket PacketEntityTeleport   where packetTag = const 0x22
+instance AnyPacket PacketAttachEntity     where packetTag = const 0x27
 instance AnyPacket PacketPreChunk         where packetTag = const 0x32
 instance AnyPacket PacketMapChunk         where packetTag = const 0x33
 instance AnyPacket PacketMultiBlockChange where packetTag = const 0x34
